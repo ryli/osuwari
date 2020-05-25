ff7232fd871f86a694a3b161ee4d03deee132f9a
yaf <lov.2014@gmail.com>
2020-05-25 13:24:40 +0800

    refactor: lint

--- wifi/index.ts
+++ wifi/index.ts
@@ -9,24 +9,24 @@ try {
     throw new Error('Username or Password must be provided!')
   }
 
-	await spinner.start('logging in..')
+  await spinner.start('logging in..')
 
-	await login(username, password)
+  await login(username, password)
 
-	await spinner.setText('network connected.')
+  await spinner.setText('network connected.')
 
-	const isOpen = await testConnection()
+  const isOpen = await testConnection()
 
-	await spinner.stop()
+  await spinner.stop()
 
   if (isOpen) {
-		console.log(hi())
+    console.log(hi())
   } else {
-		console.error('login failed!')
-	}
+    console.error('login failed!')
+  }
 } catch (e) {
-	await spinner.stop()
-	console.error(e)
+  await spinner.stop()
+  console.error(e)
 }
 
 async function login(username: string, password: string) {
@@ -46,78 +46,78 @@ async function login(username: string, password: string) {
     "method": "POST",
     "mode": "cors",
     "credentials": "omit"
-	})
+  })
 
-	if (res.ok) {
-		const text = await res.text()
-		// 还是转成对象吧
-		const body = JSON.parse(text.replace(/'/g, '"'))
-		return !!body?.success
-	}
+  if (res.ok) {
+    const text = await res.text()
+    // 还是转成对象吧
+    const body = JSON.parse(text.replace(/'/g, '"'))
+    return !!body?.success
+  }
 
   throw new Error('Network response was not ok.')
 }
 
 async function testConnection() {
-	const url = 'https://www.baidu.com/'
-	const res = await fetch(url)
-	return res.ok
+  const url = 'https://www.baidu.com/'
+  const res = await fetch(url)
+  return res.ok
 }
 
 function hi() {
-	const words = [
-		'osu!',
-		'gahaha!',
-		'moshimoshi?',
-		'hentai!',
-		"dame!",
-		'yada!',
-		'kimochi..',
-		'baka!',
-		'nande?',
-		'yamedei!',
-		'arigato!',
-		'urusai!'
-	]
-	return words[Math.floor(Math.random() * words.length)]
+  const words = [
+    'osu!',
+    'gahaha!',
+    'moshimoshi?',
+    'hentai!',
+    "dame!",
+    'yada!',
+    'kimochi..',
+    'baka!',
+    'nande?',
+    'yamedei!',
+    'arigato!',
+    'urusai!'
+  ]
+  return words[Math.floor(Math.random() * words.length)]
 }
 
 // copy from source
 function doEncryptRc4(src: string, password: string) {
-	src = (src+'').trim();
-	password = password + '';
-	var i, j = 0, a = 0, b = 0, c = 0, temp;
-	var plen = password.length,
-		size = src.length;
+  src = (src + '').trim();
+  password = password + '';
+  var i, j = 0, a = 0, b = 0, c = 0, temp;
+  var plen = password.length,
+    size = src.length;
 
-	var key = Array(256); //int
-	var sbox = Array(256); //int
-	var output = Array(size); //code of data
-	for (i = 0; i < 256; i++) {
-		key[i] = password.charCodeAt(i % plen);
-		sbox[i] = i;
-	}
-	for (i = 0; i < 256; i++) {
-		j = (j + sbox[i] + key[i]) % 256;
-		temp = sbox[i];
-		sbox[i] = sbox[j];
-		sbox[j] = temp;
-	}
-	for (i = 0; i < size; i++) {
-		a = (a + 1) % 256;
-		b = (b + sbox[a]) % 256;
-		temp = sbox[a];
-		sbox[a] = sbox[b];
-		sbox[b] = temp;
-		c = (sbox[a] + sbox[b]) % 256;
-		temp = src.charCodeAt(i) ^ sbox[c];//String.fromCharCode(src.charCodeAt(i) ^ sbox[c]);
-		temp = temp.toString(16);
-		if (temp.length === 1) {
-			temp = '0' + temp;
-		} else if (temp.length === 0) {
-			temp = '00';
-		}
-		output[i] = temp;
-	}
-	return output.join('');
+  var key = Array(256); //int
+  var sbox = Array(256); //int
+  var output = Array(size); //code of data
+  for (i = 0; i < 256; i++) {
+    key[i] = password.charCodeAt(i % plen);
+    sbox[i] = i;
+  }
+  for (i = 0; i < 256; i++) {
+    j = (j + sbox[i] + key[i]) % 256;
+    temp = sbox[i];
+    sbox[i] = sbox[j];
+    sbox[j] = temp;
+  }
+  for (i = 0; i < size; i++) {
+    a = (a + 1) % 256;
+    b = (b + sbox[a]) % 256;
+    temp = sbox[a];
+    sbox[a] = sbox[b];
+    sbox[b] = temp;
+    c = (sbox[a] + sbox[b]) % 256;
+    temp = src.charCodeAt(i) ^ sbox[c];//String.fromCharCode(src.charCodeAt(i) ^ sbox[c]);
+    temp = temp.toString(16);
+    if (temp.length === 1) {
+      temp = '0' + temp;
+    } else if (temp.length === 0) {
+      temp = '00';
+    }
+    output[i] = temp;
+  }
+  return output.join('');
 }
